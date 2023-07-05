4e5f174a62c692f6e14f565b6f2cb14e7719be84
hardiksuthar74 <hardiksuthar74@gmail.com>
2023-07-05 21:49:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+// 1) MIDDLEWARE
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+// 2) ROUTE
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongoDB connected"));
+
+// 1) START SERVER
+// const port = 3000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port: ${process.env.PORT}...`);
+});
